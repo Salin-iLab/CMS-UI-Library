fd50a63177cdf1fedff8beb60e695a9f3ae6bfed
Ycow <skypnal12@gmail.com>
2023-11-22 17:18:02 +0900

    feat: Spacing styling add

--- packages/cms-ui/src/components/Spacing/Spacing.styled.ts
+++ packages/cms-ui/src/components/Spacing/Spacing.styled.ts
@@ -1,4 +1,8 @@
 import styled from '@emotion/styled';
 import { SpacingStyledProps } from './Spacing.types';
 
-export const StyledSpacing = styled.div<SpacingStyledProps>``;
+export const StyledSpacing = styled.div<SpacingStyledProps>`
+	flex: none;
+	width: ${({ size, direction }) => (direction === 'horizontal' ? `${size}px` : undefined)};
+	height: ${({ size, direction }) => (direction === 'vertical' ? `${size}px` : undefined)};
+`;
