ce389dfd1484cd67e989b987d78054c059c0f614
Ycow <skypnal12@gmail.com>
2023-11-28 18:03:29 +0900

    fix: React import delete and Spacing stories Template add

--- packages/cms-ui/src/stories/components/Button.stories.tsx
+++ packages/cms-ui/src/stories/components/Button.stories.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
 import { Button } from '../../components/Button';
 import { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta = {
 	title: 'components/Button',
 	component: Button,
+	argTypes: {
+		customCSS: {
+			control: {
+				type: null,
+			},
+		},
+	},
 };
 
 type Story = StoryObj<typeof Button>;
@@ -12,21 +18,21 @@ type Story = StoryObj<typeof Button>;
 export const SmallButton: Story = {
 	args: {
 		onClick: () => alert('clicked'),
-		children: <span>SmallButton</span>,
+		children: 'SmallButton',
 		size: 'sm',
 	},
 };
 export const MiddleButton: Story = {
 	args: {
 		onClick: () => alert('clicked'),
-		children: <span>MiddleButton</span>,
+		children: 'MiddleButton',
 		size: 'md',
 	},
 };
 export const LargeButton: Story = {
 	args: {
 		onClick: () => alert('clicked'),
-		children: <span>LargeButton</span>,
+		children: 'LargeButton',
 		size: 'lg',
 	},
 };
--- packages/cms-ui/src/stories/components/Spacing.stories.tsx
+++ packages/cms-ui/src/stories/components/Spacing.stories.tsx
@@ -1,36 +1,51 @@
-import { Meta, StoryObj } from '@storybook/react';
+import { Meta, StoryFn, StoryObj } from '@storybook/react';
 import React from 'react';
-import { PropsWithChildren } from 'react';
 import { Spacing } from '../../components/Spacing';
 
-const meta: Meta = {
-	title: 'components/Spacing',
-	component: Spacing,
-};
+type Story = StoryObj<typeof Spacing>;
 
-interface WrapperProps {
-	direction: 'horizontal' | 'vertical';
-}
+const Template: StoryFn<typeof Spacing> = args => {
+	const flexDirection = args?.direction === 'horizontal' ? 'row' : 'column';
 
-const Wrapper = ({ children, direction }: PropsWithChildren<WrapperProps>) => {
-	const flexDirection = direction === 'horizontal' ? 'row' : 'column';
 	return (
 		<div style={{ display: 'flex', flexDirection, border: '1px solid red' }}>
-			<div style={{ background: '#EEEEEE', border: '1px solid #DDDDDD', padding: 20 }}>박스1</div>
-			{children}
-			<div style={{ background: '#EEEEEE', border: '1px solid #DDDDDsD', padding: 20 }}>박스2</div>
+			<div style={{ background: '#EEEEEE', border: '1px solid #DDDDDsD', padding: 20 }}>BOX 1</div>
+			<Spacing {...args} />
+			<div style={{ background: '#EEEEEE', border: '1px solid #DDDDDsD', padding: 20 }}>BOX 2</div>
 		</div>
 	);
 };
 
-type Story = StoryObj<typeof Spacing>;
-
-export const Horizontal: Story = {
-	render: args => (
-		<Wrapper direction={args.direction || 'vertical'}>
-			<Spacing {...args} />
-		</Wrapper>
-	),
+const meta: Meta<typeof Spacing> = {
+	title: 'components/Spacing',
+	component: Spacing,
 };
 
 export default meta;
+
+export const Defualt: Story = {
+	render: Template,
+	args: {
+		direction: 'vertical',
+		size: 30,
+	},
+};
+
+// const Wrapper = ({ direction }: PropsWithChildren<WrapperProps>) => {
+// 	const flexDirection = direction === 'horizontal' ? 'row' : 'column';
+// 	return (
+// 		<div style={{ display: 'flex', flexDirection, border: '1px solid red' }}>
+// 			<div style={{ background: '#EEEEEE', border: '1px solid #DDDDDD', padding: 20 }}>박스1</div>
+// 			{/* {children} */}
+// 			<div style={{ background: '#EEEEEE', border: '1px solid #DDDDDsD', padding: 20 }}>박스2</div>
+// 		</div>
+// 	);
+// };
+
+// export const Horizontal: Story = {
+// 	render: args => (
+// 		<Wrapper direction={args.direction || 'vertical'}>
+// 			<Spacing {...args} />
+// 		</Wrapper>
+// 	),
+// };
