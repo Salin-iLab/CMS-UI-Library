bb86c25f6ee840c15b8e6ac7a88ab72a1bf2fc2d
Ycow <skypnal12@gmail.com>
2023-12-09 10:08:19 +0900

    feat: web sidebar router 추가 및 테스팅중

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,19 @@
-import { Button } from '@salin/cms-ui';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import TestPage from './pages/TestPage';
+import './App.css';
+import Layout from './components/Layout';
 
 function App() {
 	return (
-		<>
-			<Button
-				size="md"
-				types="outline"
-				colorTypes="blue"
-				customCSS={{
-					marginBottom: '10px',
-				}}>
-				Test
-			</Button>
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<HomePage />} />
+					<Route path="/test" element={<TestPage />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
